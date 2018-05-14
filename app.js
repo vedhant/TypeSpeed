be608e8be9335a9a1984314bafaa164d7275defa
vedhant <vedhantagarwal2@gmail.com>
2018-05-14 13:26:25 +0530

    styling pending

---
+++ app.js
@@ -0,0 +1,49 @@
+var time = 0;
+text = document.querySelector('#text').textContent;
+var stop = false;
+text_area = document.querySelector('.type_space');
+var back = 0;
+var seconds = 0;
+
+function startTimer(){
+    window.setInterval(function(){time++;},100);
+}
+
+text_area.addEventListener('keydown',function(e){
+    if(e.key != "Shift"){
+    var text_typed = e.target.value;
+    len = text_typed.length;
+    back = 0;
+    if(!time){
+        startTimer();
+        text_area.style.background = "rgba(0,255,0,0.7)";}
+    if(e.key=="Backspace"){
+        stop = false;
+        back =1;
+        text_area.style.background = "rgba(0,255,0,0.7)";
+    }
+    if(stop==true){
+        
+        e.preventDefault();
+    }
+    
+    if(e.key != text[len] && back==0){
+        stop = true;
+        text_area.style.background = "rgba(255,0,0,0.7)";
+    }
+    if(e.key == text[text.length-1] && len == text.length-1)
+        {
+            seconds = time/10;
+            document.querySelector('div').innerHTML = "<input>";
+            document.querySelector('div input').setAttribute('type','text');
+            document.querySelector('div input').setAttribute('value',text);
+            document.querySelector('div input').setAttribute('class','type_space');
+            document.querySelector('div').innerHTML += "<p>Your typing speed is " + seconds/text.length + " seconds per character</p>";
+            document.querySelector('div input').style.background = "rgba(0,255,0,0.7)";
+        }
+    }
+});
+
+document.querySelector('.try_again').addEventListener('click',function(){
+   location.reload(); 
+});
