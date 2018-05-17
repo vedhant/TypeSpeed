91334acab7e2056d7bef86c76edd4313f13ead6b
vedhant <vedhantagarwal2@gmail.com>
2018-05-17 11:08:33 +0530

    styling done

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ text_area.addEventListener('keydown',function(e){
         text_area.style.background = "rgba(0,255,0,0.7)";
     }
     if(stop==true){
-        
+
         e.preventDefault();
     }
-    
+
     if(e.key != text[len] && back==0){
         stop = true;
         text_area.style.background = "rgba(255,0,0,0.7)";
@@ -34,16 +34,16 @@ text_area.addEventListener('keydown',function(e){
     if(e.key == text[text.length-1] && len == text.length-1)
         {
             seconds = time/10;
-            document.querySelector('div').innerHTML = "<input>";
-            document.querySelector('div input').setAttribute('type','text');
-            document.querySelector('div input').setAttribute('value',text);
-            document.querySelector('div input').setAttribute('class','type_space');
-            document.querySelector('div').innerHTML += "<p>Your typing speed is " + seconds/text.length + " seconds per character</p>";
-            document.querySelector('div input').style.background = "rgba(0,255,0,0.7)";
+            document.querySelector(' div div').innerHTML = "<input>";
+            document.querySelector('div div input').setAttribute('type','text');
+            document.querySelector('div div input').setAttribute('value',text);
+            document.querySelector('div div input').setAttribute('class','type_space');
+            document.querySelector('div div').innerHTML += "<p>Your typing speed is " + seconds/text.length + " seconds per character</p>";
+            document.querySelector('div div input').style.background = "rgba(0,255,0,0.7)";
         }
     }
 });
 
 document.querySelector('.try_again').addEventListener('click',function(){
-   location.reload(); 
+   location.reload();
 });
